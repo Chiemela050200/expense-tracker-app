7d99eeaf668bb9d3dd9f6e428f46ce79a9dd5267
Chiemela050200 <123215673+Chiemela050200@users.noreply.github.com>
2024-08-22 21:29:25 -0400

    creating database using neon and updating the sql editor

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import React from 'react';
+
 const HomePage = () => {
-  return ( <main>
-    <h1>
-      Expemse Tracker
-    </h1>
-    </main> );
+  return (
+    <main>
+      <h1>Expense Tracker</h1>
+    </main>
+  );
 }
- 
+
 export default HomePage;
